a1d9bb352806dff4f840e3326fa15568d9d96421
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-10-30 12:30:02 -0400

    Update and rename p2.py to p4.py
    api to find HK stock price and name based on 4 digit stock code

--- p4.py
+++ p4.py
@@ -2,11 +2,12 @@ import sys
 import urllib
 import re
 
-def get_quote(symbol):
-    symbol = format(symbol, '0004d')
+def get_quote():
+    symbol = format(sys.argv[0], '0004d')
     base_url = 'https://hk.finance.yahoo.com/lookup?s='
     content = urllib.urlopen(base_url + symbol +'.hk').read()
     m = re.search('id="ref_(.*?)">(.*?)<', content)
+    name = m.group(1)
     if m:
         quote = m.group(2)
         name = m.group(1)
