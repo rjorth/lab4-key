a8a173243af6c9a6bc8822add700f6529e6c7701
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-10-30 23:50:34 -0400

    Update p5.py
    A button to convert entries from a number to a roman numeral and a roman numeral to a number.

--- p5.py
+++ p5.py
@@ -1,36 +1,52 @@
 from Tkinter import *
 root = Tk()
 
+def check_value():
+    if e1.get() == int(e1.get()):
+        to_roman(e1.get())
+    else:
+        from_roman(e1.get())
 
-def convert_number(n):
-    if n < 1: return "Number is out of range"
-    if n == 1: return "I"
-    if n == 4: return "IV"
-    if n == 5: return "V"
-    if n == 9:  return "IX"
-    if n == 10: return "X"
-    if n == 40: return "XL"
-    if n == 50: return "L"
-    if n == 90: return "XC"
-    if n == 100: return "C"
-    if n == 400: return "CD"
-    if n == 500: return "D"
-    if n == 900: return "CM"
-    if n == 1000: return "M"
-    if n == 1400: return "MCD"
-    if n == 1500: return "MD"
-    if n == 1900: return "MCM"
-    if n > 9999: return "Number is out of range"
+roman_numeral_map = (('MCM', 1900),
+                   ('MD', 1500),
+                   ('MCD', 1400),
+                   ('M',  1000),
+                   ('CM', 900),
+                   ('D',  500),
+                   ('CD', 400),
+                   ('C',  100),
+                   ('XC', 90),
+                   ('L',  50),
+                   ('XL', 40),
+                   ('X',  10),
+                   ('IX', 9),
+                   ('V',  5),
+                   ('IV', 4),
+                   ('I',  1))
 
-    for i in [1000, 100, 10, 1]:
-        for j in [9 * i, 5 * i, 4 * i, i]:
-            if n >= j:
-                return convert_number(j) + convert_number(n - j)
+def to_roman(n):
+    result = ""
+    for numeral, integer in roman_numeral_map:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    print result
+
+roman_numeral_pattern = '^M?M?M?(CM|CD|D?C?C?C?)(XC|XL|L?X?X?X?)(IX|IV|V?I?I?I?)$'
+
+def from_roman(s):
+    result = 0
+    index = 0
+    for numeral, integer in roman_numeral_map:
+        while s[index:index+len(numeral)] == numeral:
+            result += integer
+            index += len(numeral)
+    return result
 
 t1=Label(root, text="Convert")
 t1.grid(row=0, column=0)
 e1 = Entry(root, bd = 10)
 e1.grid(row=0, column=1)
-converter_button = Button(root, text="Enter something", width=20, command=convert_number)
+converter_button = Button(root, text="Enter something", width=20, command=check_value)
 converter_button.grid(row=1, column=1)
 root.mainloop()
